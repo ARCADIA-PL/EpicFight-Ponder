33a08060376a9238849947202a7335e6429b97e8
Arcadia <1919463065@qq.com>
2026-03-27 02:19:57 +0800

    环境配置

---
+++ src/main/java/org/com/efp/EpicFightPonder.java
@@ -0,0 +1,38 @@
+package org.com.efp;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(EpicFightPonder.MOD_ID)
+public class EpicFightPonder {
+    public static final String MOD_ID = "epic_fight_ponder";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public EpicFightPonder(FMLJavaModLoadingContext context) {
+        IEventBus bus = context.getModEventBus();
+        bus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
